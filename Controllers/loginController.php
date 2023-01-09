8d0a94583f2f3f5db5a5ffa255112d52624e8cee
tfermoso <tomas.fermoso@gmail.com>
2023-01-09 18:01:26 +0100

    add LoginController

---
+++ Controllers/loginController.php
@@ -0,0 +1,10 @@
+<?php
+
+class LoginController{
+    public function index(){
+        echo "hola desde index del Login";
+    }
+    public function form(){
+        echo "hola desde el form del Login";
+    }
+}
