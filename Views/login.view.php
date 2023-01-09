4de059ab3e7134ed3293a8457ac1156d475e1150
jtecoia <jtecoia@gmail.com>
2023-01-09 20:20:17 +0100

    creando register.view.php

--- Views/login.view.php
+++ Views/login.view.php
@@ -1,22 +1,28 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="<?= URL_PATH ?>/Assets/css/login.css">
-    <script src="<?= URL_PATH ?>/Assets/js/login.js"></script>
+    <link rel="stylesheet" href="<?= URL_PATH ?>/Assets/css/register.css">
+   
 </head>
+
 <body>
-    <h1>Login</h1> 
-    <form action=":/login/form" method="post">
-        <label for="">Usuario</label>
-        <input type="text" name="nombre" id="">
-        <label for="">Password</label>
-        <input type="password" name="Password" id="">
-        <input type="submit" value="Login">
-        
-    </form>   
+    <div class="contenedor">
+        <h1>Login</h1>
+        <form class="formulario_registro" action="" method="post">
+            <label for="">Usuario</label>
+            <input type="text" name="nombre" id="">
+            <label for="">Password</label>
+            <input type="password" name="password" id="">
+            <input type="submit" value="Login">
+        </form>
+        <a href="./login/register">Si no tienes cuenta, registraté aquí</a>
+    </div>
 </body>
+
 </html>
