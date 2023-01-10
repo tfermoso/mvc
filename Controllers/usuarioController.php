b1e32cebf94079d951101fa2b2d03863db948639
Polsax <sonpablo@yahoo.com>
2023-01-10 18:01:20 +0100

    antes del commit

---
+++ Controllers/usuarioController.php
@@ -0,0 +1,11 @@
+<?php
+
+class usuarioController{
+    public function index(){
+        if(isset($_POST["nombre"])){
+            echo "vista usuario index";
+       
+    public function ver()
+       echo"viendo usuario);}
+    }
+}
