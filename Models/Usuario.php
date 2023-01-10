f186a09327fd87e3b02ed2285696c8a408a227e3
Polsax <sonpablo@yahoo.com>
2023-01-10 19:28:31 +0100

    actualizar

--- Models/Usuario.php
+++ Models/Usuario.php
@@ -3,6 +3,9 @@
 class Usuario extends Orm{
     public function __construct($conn)
     {
-        parent::__construct("id","usuarios",$conn);
+        parent::__construct("id","usuario",$conn);
     }
 }
+// si quieres crear una nueva tabla en base de datos vas a usuarios php
+//copias el codigo de arriba y cambias nombre usuario(tabla)por el nombre de la nueva 
+//tabla creada
