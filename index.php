c8da6237402d3e807b84ece8d5a45a13317114ea
santigc15 <gomezcacheiro@gmail.com>
2023-01-10 17:09:12 +0100

    corregida

--- index.php
+++ index.php
@@ -4,12 +4,7 @@ require_once("router.php");
 require_once("./Models/database.php");
 require_once("./Models/Orm.php");
 
-$conn=new Database();
-$orm=new Orm(1,"profesores", $conn->getConnection());
-echo "<pre>";
-var_dump($orm->getAll());
-echo "</pre>";
-exit;
+
 
 $ruta=isset($_GET["route"])?$_GET["route"]:"";
 $route=new Router($ruta);
