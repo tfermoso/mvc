dc970f80b898b2dd482c9b8a514c0ff047a2981a
tfermoso <tomas.fermoso@gmail.com>
2023-01-10 17:16:37 +0100

    add method getById

--- Models/Orm.php
+++ Models/Orm.php
@@ -13,10 +13,14 @@ class Orm{
     }
 
     public function getAll(){
-
         $stm=$this->db->prepare("SELECT * FROM {$this->tabla}");
         $stm->execute();
         return $stm->fetchAll();
-   
     }   
+    public function getById($id){
+        $stm=$this->db->prepare("select * from {$this->tabla} where id=:id");
+        $stm->bindValue(":id",$id);
+        $stm->execute();
+        return $stm->fetch();
+    }
 }
--- index.php
+++ index.php
@@ -4,7 +4,12 @@ require_once("router.php");
 require_once("./Models/Database.php");
 require_once("./Models/Orm.php");
 
-
+$conn=new Database();
+$orm=new Orm(15,"profesores",$conn->getConnection());
+echo "<pre>";
+var_dump($orm->getById(15));
+echo "</pre>";
+exit;
 
 
 $ruta=isset($_GET["route"])?$_GET["route"]:"";
