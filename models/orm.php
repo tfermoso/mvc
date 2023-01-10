86e372f7b8237b0602c7637748ac781c806fe59f
JessmannPengard <contacto@jessmann.com>
2023-01-10 18:03:13 +0100

    added orm insert

--- models/orm.php
+++ models/orm.php
@@ -19,4 +19,39 @@ class Orm
         $stm->execute();
         return $stm->fetchAll();
     }
+
+    public function getById($id)
+    {
+        $stm = $this->dbconn->prepare("SELECT * FROM {$this->table} WHERE id=:id");
+        $stm->bindValue(":id", $id);
+        $stm->execute();
+        return $stm->fetch();
+    }
+
+    public function deleteById($id)
+    {
+        $stm = $this->dbconn->prepare("DELETE FROM {$this->table} WHERE id=:id");
+        $stm->bindValue(":id", $id);
+        $stm->execute();
+    }
+
+    public function insert($data)
+    {
+        $sql = "INSERT INTO {$this->table} ";
+        $keys = "(";
+        $values = "VALUES (";
+
+        foreach ($data as $key => $value) {
+            $keys .= "{$key},";
+            $values .= ":{$key},";
+        }
+        $keys = substr($keys, 0, -1) . ")";
+        $values = substr($values, 0, -1) . ")";
+        $sql .= $keys . $values;
+        $stm = $this->dbconn->prepare($sql);
+        foreach ($data as $key => $value) {
+            $stm->bindValue(":{$key}", $value);
+        }
+        $stm->execute();
+    }
 }
