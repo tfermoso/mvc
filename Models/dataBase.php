391a08bb4913ca4edfd39a66754b620203dcb724
Rey-86 <reygranhacristian@gmail.com>
2023-01-10 17:14:15 +0100

    cambios y consulta base datos

--- Models/dataBase.php
+++ Models/dataBase.php
@@ -1,17 +1,26 @@
 <?php
 
-class Database{
+class Database
+{
     private $connection;
-    public function __construct()
 
+    public function __construct()
     {
         try {
-            $url_conexion = ("mysql:host=".HOST.;dbname=$dbname", $username, $password);
+            $url_conexion = "mysql:host=" . HOST . ";dbname=" . DBNAME;
+            $this->connection = new PDO($url_conexion, DB_USER, DB_PASSWORD);
             
-        } 
-        catch (PDOException $pe) {
-           
-        }  
+        } catch (PDOException $pe) {
+            $this->connection = null;
+            die("error al conectarme");
+        }
+    }
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+    public function closeConnection()
+    {
+        $this->connection = null;
     }
 }
-?>
