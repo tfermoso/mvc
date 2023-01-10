43774843c93851fc4456db0bc5ef8f48f46348fe
Polsax <sonpablo@yahoo.com>
2023-01-10 20:11:05 +0100

    corregido loginController

--- Controllers/loginControler.php
+++ Controllers/loginControler.php
@@ -1,22 +1,16 @@
 <?php
 
 class LoginController{
-    public function index(){
-        if(isset($_POST["nombre"])){
-class LoginController
-{
+
     public function index()
     {
         if (isset($_POST["nombre"])) {
             echo "recibiendo login";
         }else{
             require_once(__DIR__.'./../Views/login.view.php');
-        } else {
-            require_once(__DIR__ . './../Views/login.view.php');
         }
     }
-    public function register(){
-        require_once(__DIR__.'./../Views/register.view.php');
+
     public function register()
     {
         if (isset($_POST["nombre"])) {
