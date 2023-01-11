2b2c73c8bd0596e85a91ed2fa775f2b24e82e939
achorro55 <achorro55@gmail.com>
2023-01-11 19:30:44 +0100

    CAMBIAS

---
+++ Controllers/adminController.php
@@ -0,0 +1,10 @@
+<?php
+
+class adminController{
+    public function index(){
+        echo " vista usuario index del admin";
+    }
+    public function ver(){
+        echo "viendo un usuario del admin";
+    }
+}
