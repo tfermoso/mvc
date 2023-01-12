fa1212cf3e580f075ea089b10188ddb6744f0b85
santigc15 <gomezcacheiro@gmail.com>
2023-01-12 19:05:17 +0100

    msj

---
+++ Models/mensaje.php
@@ -0,0 +1,32 @@
+<?php
+
+class Mensaje extends Orm {
+
+    public function __construct($conn)
+    {
+        parent::__construct("id", "mensajes", $conn);
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
