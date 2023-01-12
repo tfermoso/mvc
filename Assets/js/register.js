2bd9f567062d5590ad1f1e4f4fc34daef89fc278
tfermoso <tomas.fermoso@gmail.com>
2023-01-12 16:01:59 +0100

    add register js

--- Assets/js/register.js
+++ Assets/js/register.js
@@ -1,24 +1,15 @@
-window.onload=function(){
-    /*
-    let formulario=document.getElementById("registerForm");
-    
-    formulario.onsubmit=function(e){
-        
-        let pass=document.getElementById("password").value;
-        let repass=document.getElementById("repassword").value;
-        if(pass==repass){
+window.onload = function () {
+    let formulario = document.getElementById("registerForm");
+    formulario.onsubmit = function (e) {
+
+        let pass = document.getElementById("password").value;
+        let repass = document.getElementById("repassword").value;
+        if (pass == repass) {
             //Envío el formulario
-        }else{
+        } else {
             e.preventDefault();
             alert("Las contraseñas son distintas");
         }
 
-    }*/
-    let formulario=$("#registerForm");
-    formulario.submit((e)=>{
-        if($("#password").val()!=$("#repassword").val()){
-            e.preventDefault();
-            alert("Las contraseñas son distintas");
-        }
-    })
+    }
 }
