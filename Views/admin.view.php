16e785d78fbd54e80cc04083622d74ab28f8c606
santigc15 <gomezcacheiro@gmail.com>
2023-01-12 18:14:51 +0100

    user

--- Views/admin.view.php
+++ Views/admin.view.php
@@ -176,7 +176,7 @@
           <img src="<?=URL_PATH ?>/Assets/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?$user_name?></a>
+          <a href="#" class="d-block"><?=$user_name?></a>
         </div>
       </div>
 
