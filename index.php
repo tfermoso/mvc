d6cb5db16379fecbc0283f6d3e58d6c76e4995c6
tfermoso <tomas.fermoso@gmail.com>
2023-01-13 19:10:34 +0100

    creando admin layout

--- index.php
+++ index.php
@@ -5,24 +5,12 @@ require_once("./Models/Database.php");
 require_once("./Models/Orm.php");
 require_once("./Models/Usuario.php");
 require_once("./Models/Mensaje.php");
-
-/*
-
-$conn=new Database();
-$usr=new Usuario($conn->getConnection());
-$datos=array();
-$datos["nombre"]="Manolo";
-$datos["email"]="manolo@gmail.com";
-$datos["password"]="123445";
-
-$usr->deleteById(1);
+require_once("./controllers/controller.php");
 
 
-echo "<pre>";
-var_dump($usr->getAll());
-echo "</pre>";
-exit;
-*/
+
+
+
 $ruta=isset($_GET["route"])?$_GET["route"]:"";
 $route=new Router($ruta);
 $route->run();
