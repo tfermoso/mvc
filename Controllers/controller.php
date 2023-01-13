0cb34bba384182dd6590a3377f81a3083340e498
santigc15 <gomezcacheiro@gmail.com>
2023-01-13 17:43:01 +0100

    layout

---
+++ Controllers/controller.php
@@ -0,0 +1,20 @@
+<?php
+class Controller{
+
+
+protected function render($path,$parameter="",$layout=""){
+
+ob_start();
+require_once(__DIR__."./../Views/".$path.".view.php");
+$content=ob_get_clean();
+require_once(__DIR__."./../Views/".$layout.".layout.php");
+
+
+
+
+}
+
+
+
+
+}
