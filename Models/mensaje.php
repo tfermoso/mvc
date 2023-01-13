fb5b02e459e1b5130c0551944149a707d1a747d6
santigc15 <gomezcacheiro@gmail.com>
2023-01-13 17:22:51 +0100

    layout

--- Models/mensaje.php
+++ Models/mensaje.php
@@ -6,7 +6,7 @@ class Mensaje extends Orm{
         parent::__construct("id", "mensajes", $conn);
     }
     public function getAllByIdUserDestino($id){
-        $stm=$this->db->prepare("SELECT U.nombre,M.mensaje,M.fecha_enviio  FROM mensajes as M inner join usuarios as U on M.id_usuario_origen=U.id  where id_usuario_destino=:id");
+        $stm=$this->db->prepare("SELECT U.nombre,M.mensaje,M.fecha_envio  FROM mensajes as M inner join usuarios as U on M.id_usuario_origin=U.id  where id_usuario_destino=:id");
         $stm->bindValue(":id",$id);
         $stm->execute();
         return $stm->fetchAll();
