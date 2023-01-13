78c61b080696d105a128da42e4f77eb83e1ccdf8
santigc15 <gomezcacheiro@gmail.com>
2023-01-13 19:19:32 +0100

    msg

--- Views/admin/admin.view.php
+++ Views/admin/admin.view.php
@@ -1,1 +1,5 @@
-<h1>Vista principal de Admin</h1>
+      <!-- Main content -->
+      <section class="content">
+        <h2>Vista principal</h2>
+      </section>
+      <!-- /.content -->
--- Views/layout/admin.layout.php
+++ Views/layout/admin.layout.php
@@ -809,10 +809,6 @@
       <!-- /.sidebar -->
     </aside>
 
-<?php echo $content; ?>
-
-
-
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
       <!-- Content Header (Page header) -->
@@ -833,7 +829,16 @@
       </div>
       <!-- /.content-header -->
 
+<?php echo $content; ?>
 
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
 
     <!-- Main Footer -->
     <footer class="main-footer">
