d6849c69dc11e89a5822a3dff07d48d1d2f52bcd
santigc15 <gomezcacheiro@gmail.com>
2023-01-13 17:43:08 +0100

    seguirdo

--- Views/layout/admin.layout.php
+++ Views/layout/admin.layout.php
@@ -809,6 +809,10 @@
       <!-- /.sidebar -->
     </aside>
 
+<?php echo $content; ?>
+
+
+
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
       <!-- Content Header (Page header) -->
@@ -829,19 +833,7 @@
       </div>
       <!-- /.content-header -->
 
-      <!-- Main content -->
-      <section class="content">
 
-      </section>
-      <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
-
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-      <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
 
     <!-- Main Footer -->
     <footer class="main-footer">
