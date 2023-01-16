483fa958cab167542ae135a19f3695ae57e9e4ab
santigc15 <gomezcacheiro@gmail.com>
2023-01-16 18:42:33 +0100

    finapi

--- Controllers/apiController.php
+++ Controllers/apiController.php
@@ -1,22 +1,40 @@
 <?php
 
-class ApiController{
+class ApiController
+{
 
-    public function index(){
+    public function index()
+    {
 
-        $datos=array();
-        $conn=new Database();
-        $usr=new Mensaje($conn->getConnection());
+        $datos = array();
+        $conn = new Database();
+        $usr = new Mensaje($conn->getConnection());
 
         echo json_encode($usr->getAllApi());
-
     }
-public function message($id=""){
+    public function message($id = "")
+    {
 
- 
-    $datos=array();
-    $conn=new Database();
-    $usr=new Mensaje($conn->getConnection());
-    echo json_encode($usr->getAllByIdUserDestinoApi($id));
-}
+
+        $datos = array();
+        $conn = new Database();
+        $usr = new Mensaje($conn->getConnection());
+        echo json_encode($usr->getAllByIdUserDestinoApi($id));
+    }
+
+    public function new()
+    {
+      
+        $conn = new Database();
+        $msg = new Mensaje($conn->getConnection());
+        $datos = array();
+        $datos["id_usuario_origen"] = $_POST["id_usuario_origen"];
+        $datos["id_usuario_destino"] = $_POST["id_usuario_destino"];
+        $datos["mensaje"] = $_POST["mensaje"];
+        $msg->insertar($datos);
+        var_dump($_POST);
+        $respuesta=array();
+        $respuesta["msg"]="mensaje enviado";
+        echo json_encode($respuesta);
+    }
 }
