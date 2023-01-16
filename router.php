7dd39c3ee7379b4befe152c1cc56eb4f7ce90d5b
tfermoso <tomas.fermoso@gmail.com>
2023-01-16 16:44:58 +0100

    creando apiController

--- router.php
+++ router.php
@@ -1,48 +1,55 @@
 <?php
 
-class Router{
+class Router
+{
     private $controller;
     private $method;
+    private $param;
     private $ruta;
 
-    public function __construct($ruta="")
+    public function __construct($ruta = "")
     {
-        $this->ruta=$ruta;
+        $this->ruta = $ruta;
         $this->matchRoute();
     }
 
-    public function matchRoute(){
-        $url=explode("/",$this->ruta);
-  
-        $this->controller=($url[0]!=""?$url[0]:"Home")."Controller";
-        $this->method=isset($url[1])?$url[1]:"index";
-        
-        if($this->controller=="adminController"){
+    public function matchRoute()
+    {
+
+        $url = explode("/", $this->ruta);
+
+        $this->controller = ($url[0] != "" ? $url[0] : "Home") . "Controller";
+        $this->method = isset($url[1]) ? $url[1] : "index";
+        $this->param = isset($url[2]) ? $url[2] : null;
+
+        if ($this->controller == "adminController") {
             session_start();
-            if(! isset($_SESSION["nombre"])){
-                header("Location: ".URL_PATH."/login");
+            if (!isset($_SESSION["nombre"])) {
+                header("Location: " . URL_PATH . "/login");
                 exit;
             }
         }
 
-        $ruta_controlador=__DIR__.'/controllers/'.$this->controller.'.php';
-        if(file_exists($ruta_controlador)){
+        $ruta_controlador = __DIR__ . '/controllers/' . $this->controller . '.php';
+        if (file_exists($ruta_controlador)) {
             require_once($ruta_controlador);
-        }else{
+        } else {
             echo "page 404";
-        }  
+        }
     }
 
-    public function run(){
-        $controller=new $this->controller();
-        $method=$this->method;
-        if(method_exists($controller,$method)){
-            $controller->$method();
-        }else{
+    public function run()
+    {
+        $controller = new $this->controller();
+        $method = $this->method;
+        if (method_exists($controller, $method)) {
+            if ($this->param != null) {
+                $controller->$method($this->param);
+            } else {
+                $controller->$method();
+            }
+        } else {
             $controller->index();
         }
-        
     }
-
-
 }
