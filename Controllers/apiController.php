ddb57f858c0bbc5e6e87fc606da2d43359e9d641
LeonarEQ <leonardestupinan@yahoo.com>
2023-01-16 18:12:37 +0100

    cambios

--- Controllers/apiController.php
+++ Controllers/apiController.php
@@ -7,16 +7,17 @@ class ApiController
         $datos = array();
         $conn = new Database();
         $usr = new Mensaje($conn->getConnection());
-       
+
         echo json_encode($usr->getAllApi());
     }
 
-    public function message($id=""){
-        
-       
+    public function message($id = "")
+    {
+
+
         $datos = array();
         $conn = new Database();
-        $usr = new Mensaje($conn->getConnection());  
+        $usr = new Mensaje($conn->getConnection());
         echo json_encode($usr->getAllByIdUserDestinoApi($id));
     }
 }
