e75e9d872297b2c6a33bb601dfbf1d2165e08cba
JessmannPengard <contacto@jessmann.com>
2023-01-18 20:18:47 +0100

    api adds

--- controllers/apiController.php
+++ controllers/apiController.php
@@ -31,4 +31,29 @@ class ApiController
         $response["msg"] = "Sent message";
         echo json_encode($response);
     }
+
+    public function delete($id)
+    {
+        $db = new Database;
+        $msg = new Message($db->getConnection());
+        $msg->deleteById($id);
+        $response = array();
+        $response["msg"] = "Message deleted";
+        echo json_encode($response);
+    }
+
+    public function login()
+    {
+        $db = new Database;
+        $usr = new User($db->getConnection());
+        $logged = $usr->login($_POST["username"], md5($_POST["password"]));
+        if ($logged) {
+            $response["msg"] = "Access granted";
+            $response["logged"] = true;
+        } else {
+            $response["msg"] = "Access denied";
+            $response["logged"] = false;
+        }
+        echo json_encode($response);
+    }
 }
