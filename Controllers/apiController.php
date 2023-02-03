9d591fb4a352716d5e7cd68f3279f29281c9946d
tfermoso <tomas.fermoso@gmail.com>
2023-02-03 17:04:23 +0100

    apicontroller

--- Controllers/apiController.php
+++ Controllers/apiController.php
@@ -7,7 +7,10 @@ class ApiController
         $datos = array();
         $conn = new Database();
         $usr = new Mensaje($conn->getConnection());
-
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
         echo json_encode($usr->getAllApi());
     }
 
@@ -16,6 +19,10 @@ class ApiController
         $datos = array();
         $conn = new Database();
         $usr = new Mensaje($conn->getConnection());
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
         echo json_encode($usr->getAllByIdUserDestinoApi($id));
     }
 
@@ -28,8 +35,52 @@ class ApiController
         $datos["id_usuario_destino"] = $_POST["id_usuario_destino"];
         $datos["mensaje"] = $_POST["mensaje"];
         $msg->insertar($datos);
-        $respuesta=array();
-        $respuesta["msg"]="mensaje enviado";
+        $respuesta = array();
+        $respuesta["msg"] = "mensaje enviado";
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
         echo json_encode($respuesta);
+    }
+    public function delete($id = "")
+    {
+        $conn = new Database();
+        $msg = new Mensaje($conn->getConnection());
+        $msg->deleteById($id);
+        $respuesta["msg"] = "mensaje eliminado";
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+        echo json_encode($respuesta);
+    }
+
+    public function login()
+    {
+        $conn = new Database();
+        $usr = new Usuario($conn->getConnection());
+
+        $nombre = $_POST["nombre"];
+        $password = $_POST["password"];
+
+        $usuario = $usr->getUsuario($nombre, $password);
+        if ($usuario != null) {
+            $respuesta["msg"] = "login ok";
+            $respuesta["usuario"] = $usuario;
+            header('Access-Control-Allow-Origin: *');
+            header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+            header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+            echo json_encode($respuesta);
+        } else {
+            $respuesta["msg"] = "Usuario o contraseña incorrecta";
+            
+            header('Access-Control-Allow-Origin: *');
+            header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+            header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+            echo json_encode($respuesta);
+        }
     }
 }
